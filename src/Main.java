254d940854da7377f0528f4ed13539bf20a2b3ec
AlexDv <DvoinikovAV@yandex.ru>
2024-02-01 16:45:40 +0300

    Начало

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
+    public static void visoGod(int god)
+    {
+        if(((god%4==0)&&((god%100!=0))||(god%400==0))&&god>=1584)
+        {
+            System.out.println(god+" год - високосный");
+        }
+        else{
+            System.out.println(god+" год - не високосный");
+        }
+
+    }
     public static void main(String[] args)
 
     {
-        System.out.println("Hello world!");
+        visoGod(1600);
     }
 }
