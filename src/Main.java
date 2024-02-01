a556ffb77f6b0761db7f2238c68f7b9fb4ee7a90
AlexDv <DvoinikovAV@yandex.ru>
2024-02-01 17:26:05 +0300

    2 метод (serviceMobile)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void visoGod(int god)
     {
@@ -8,12 +10,47 @@ public class Main {
         else{
             System.out.println(god+" год - не високосный");
         }
+    }
+
+    public static void serviceMobile(int osName, int godMade)
+    {
+        int currentYear = LocalDate.now().getYear();
+        String txtVer;
+        String txtOsName;
+        switch (osName)
+        {
+            case 0:
+                txtOsName="iOS";
+                break;
+            case 1:
+                txtOsName="Android";
+                break;
+            default:
+                txtOsName="2";
+        }
+
+        if (txtOsName.equals("2"))
+        {
+            System.out.println("Такого приложения нет в списке обновляемых систем");
+        }
+        else
+        {
+            if (godMade<currentYear){
+                txtVer="Установите облегченную версию приложения для ";
+            }else{
+                txtVer="Установите версию приложения для ";
+            }
+            System.out.println(txtVer+txtOsName+" по ссылке");
+        }
+
+
 
     }
     public static void main(String[] args)
 
     {
         visoGod(1600);
+        serviceMobile(0,2024);
 
     }
 }
